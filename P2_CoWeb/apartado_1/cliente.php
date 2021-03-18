3c25ff58cd52aa1fc32d8d47cd50350fd418b62b
marcosPlaza <marcos.plaza.gonzalez@gmail.com>
2021-03-18 22:34:31 +0100

    Primer apartado acabado

--- P2_CoWeb/apartado_1/cliente.php
+++ P2_CoWeb/apartado_1/cliente.php
@@ -1,3 +1,15 @@
+<!-- GET no oculta los valores pasados por parametro -->
+<!-- POST si oculta los valores pasador por parametro en la URL -->
+
+<!-- Power Point -->
+<!--
+GET : pide al servidor una pagina o datos.Si el pedido tiene parámetros, ellos se envían por medio del URL
+como un string query
+
+POST : envía los datos a un web server y recupera la respuesta del servidor
+Si el pedido tiene parámetros, ellos se incluyen en el paquete
+HTTP pedido, y no en el URL
+-->
 <form action="http://localhost/COW/P2_CoWeb/apartado_1/servidor.php" method=POST>
     <div>
         name: <input type="text" name="var1"/> password: <input type="text" name="var2"/> <input type="submit"/>
--- P2_CoWeb/apartado_1/servidor.php
+++ P2_CoWeb/apartado_1/servidor.php
@@ -1,5 +1,30 @@
+<!-- Server en PHP -->
 <?php
-    $var1= $_POST["var1"];
-    $var2 = $_POST["var2"]; 
-    echo "Hello $var1 $var2";
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    //Recepción de datos
+    $city = $_POST["city"];
+    $trip_start = $_POST["trip_start"];
+    $trip_end = $_POST["trip_end"];
+    $num_people = $_POST["num_people"];
+
+    //Patrones que deben coincidir
+    $city_pattern = "/^([a-zA-Z]+|[a-zA-Z]+\s[a-zA-Z]+)$/i";
+    $date_pattern = "/^(20)([2-4][1-9]|50)(-)(((0)[1-9])|((1)[0-2]))(-)([0-2][0-9]|(3)[0-1])$/i";
+    $num_people_pattern = "/^([1-9]|[1-4]\d|50)$/i";
+
+    // Checking input values with regex
+    if(!preg_match($city_pattern, $city) || !preg_match($date_pattern, $trip_start) || !preg_match($date_pattern, $trip_end) || !preg_match($num_people_pattern, $num_people)){
+        header("Location: 400.html");
+    }
+
+    //Substituir por página principal
+    echo "<h2>We have found so many coincidences with the following params given:\n";
+    echo "$city\n";
+    echo "$trip_start\n";
+    echo "$trip_end\n";
+    echo "$num_people\n";
+
+}else{
+    header("Location: 405.html");
+}
 ?>
