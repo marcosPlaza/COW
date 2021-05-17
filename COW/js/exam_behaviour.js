7eefe0cac273064d10130138a67ed52d439432be
marcosplazagonzalez <marcos.plaza.gonzalez@gmail.com>
2021-05-17 12:28:52 +0200

    Antes de examen

---
+++ COW/js/exam_behaviour.js
@@ -0,0 +1,13 @@
+window.onload = function() {
+    // Implementar aqui el comportamiento de la web Cliente
+}
+
+/**
+ *  Prototype
+ *  document.observe("dom:loaded", function(){
+ *  });
+ * 
+ *  JQuery
+ *  $(document).ready(function(){
+ *  });
+ */
